d90743673b25ba4dce165f530607a849e044cab3
1ikken <97161943+1kken@users.noreply.github.com>
2025-08-20 00:28:08 +0800

    login backend

---
+++ backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Auth;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rules\Password;
+
+class AuthController extends Controller
+{
+    public function register(Request $request){
+        $validated = $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => ['required','confirmed',Password::min(8)
+                                                    ->mixedCase()
+                                                    ->letters()
+                                                    ->numbers()
+                                                    ->symbols()
+                                                    ->uncompromised()]
+        ]);
+        $user = User::create($validated);
+
+        return response()->json([
+            'user' => $user,
+        ],200);
+    }
+    public function login(Request $request){
+        $validated = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if(!Auth::attempt($validated)){
+            return response()->json(['message' => 'Invalid Credentials'],401);
+        }
+
+        $user = Auth::user();
+
+        return response()->json(['user'=>$user],200);
+    }
+
+    public function logout(Request $request){
+        Auth::logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return response()->json(['message' => 'Successfully logged out'],200);
+    }
+
+}
